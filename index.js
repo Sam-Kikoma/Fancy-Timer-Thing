2f10d116e137a288e66ad94732d99542be85d5a0
Samson Kikoma <kikomasamson@gmail.com>
2023-01-12 10:04:49 +0100

    Updated timer logic

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const durationInput = document.querySelector("#duration");
 const startBtn = document.querySelector("#start");
 const pause = document.querySelector("#pause");
+const circle = document.querySelector("circle");
+const circumference = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", circumference);
+let duration;
 
 const timer = new Timer(durationInput, startBtn, pause, {
-    onStart(){
-        console.log("Timer is live my boy");
+    onStart(totalDuration){
+        duration = totalDuration;
     },
-    onTick(){
-        console.log("Tick Tock");
+    onTick(timeRemaining){
+        circle.setAttribute("stroke-dashoffset", 
+        circumference * timeRemaining / duration - circumference
+        );
+        
     },
     onComplete(){
         console.log("Ding ding ding");
